671fb083e5c741812f4a609ea8582d2c62c7650d
Pease Adeniji <adenijiayomide13@gmail.com>
2022-12-24 18:08:14 +0100

    app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import HeroSection from './sections/HeroSection';
-import PhoneModel from './sections/PhoneModel';
+// import PhoneModel from './sections/PhoneModel';
 import Quote from './sections/Quote';
 import { GlobalStyle } from './styles/GlobalStyles';
 
