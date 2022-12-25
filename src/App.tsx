2ea4b1fe09195b2247f559d59a368f056ab50013
Pease Adeniji <adenijiayomide13@gmail.com>
2022-12-25 10:03:46 +0100

    battery section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import BatterySection from './sections/BatterySection';
 import DesignSection from './sections/DesignSection';
 import DisplaySection from './sections/DisplaySection';
 import HeroSection from './sections/HeroSection';
@@ -18,6 +19,7 @@ function App() {
       <DesignSection />
       <DisplaySection />
       <ProcessorSection />
+      <BatterySection />
     </>
   );
 }
